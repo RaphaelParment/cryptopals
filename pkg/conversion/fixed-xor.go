1e0b1e312e6dd35847fb8401c0f1205533c6fdc4
RaphaelParment <raphparment@gmail.com>
2020-01-02 22:01:13 +0000

    Implementation of Xor function used to computed the fixed-xor.

--- pkg/conversion/fixed-xor.go
+++ pkg/conversion/fixed-xor.go
@@ -2,43 +2,29 @@ package conversion
 
 import "errors"
 
+// Xor computes the xor of input against comparator
+func Xor(input byte, comparator byte) byte {
+	var output byte
+	inputBits := UByteToBits(input)
+	comparatorBits := UByteToBits(comparator)
+	
+	for i := 0; i < 8; i++ {
+		if inputBits[i] != comparatorBits[i] {
+			output += 1 << (7 - i%8)
+		}
+	}
+	return output
+}
+
 // FixedXor computes the input XORed comparator
 func FixedXor(input []byte, comparator []byte) ([]byte, error) {
-	var (
-		output []byte
-		v      byte
-		used   uint8
-	)
+	var output []byte
 	if !(len(input) == len(comparator)) {
 		return nil, errors.New("length error")
 	}
 
-	inputBits := make([]byte, len(input)*8)
-	comparatorBits := make([]byte, len(comparator)*8)
-
 	for i := 0; i < len(input); i++ {
-		bits := UByteToBits(input[i])
-		for j := 0; j < 8; j++ {
-			inputBits[j+(i*8)] = bits[j]
-		}
-
-		bits = UByteToBits(comparator[i])
-		for j := 0; j < 8; j++ {
-			comparatorBits[j+(i*8)] = bits[j]
-		}
+		output = append(output, Xor(input[i], comparator[i]))
 	}
-
-	for i := 0; i < len(inputBits); i++ {
-		if !(inputBits[i] == comparatorBits[i]) {
-			v += 1 << (7 - i%8)
-		}
-		used++
-		if used == 8 {
-			output = append(output, v)
-			v = 0
-			used = 0
-		}
-	}
-
 	return output, nil
 }
